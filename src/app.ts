5d24d5c7502fbe70bacea478884fdd21cdc9b93c
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-01-23 15:46:39 -0300

    server config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
+import ninjaRoute from "./routes/ninjas-route.js"
 
 const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.use(ninjaRoute);
 
 server.listen(4000, () => {
     console.log(`Server running!`);
